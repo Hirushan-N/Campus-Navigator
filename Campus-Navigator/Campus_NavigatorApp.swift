cd115ed6984ff148c800441313a5d93cb9c28bed
Hirushan-N <nadeeshhirushan@gmail.com>
2025-02-16 11:49:26 +0530

    Initial Commit

---
+++ Campus-Navigator/Campus_NavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Campus_NavigatorApp.swift
+//  Campus-Navigator
+//
+//  Created by Nadeesh Hirushan on 2025-02-16.
+//
+
+import SwiftUI
+
+@main
+struct Campus_NavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
