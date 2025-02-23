ee65292caeecfa2f1c6eeab70b38fe78cf7f701d
Hirushan-N <nadeeshhirushan@gmail.com>
2025-02-23 12:23:33 +0530

    test commit in dev 2

--- Campus-Navigator/ContentView.swift
+++ Campus-Navigator/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Hirushan!")
+            Text("Hello, Hirushan!!!")
         }
         .padding()
     }
